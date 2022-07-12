d52605d87d15185b5a42a32c29868948986d0f18
Christina Greenwood <greenwoodc@me.com>
2022-07-12 16:19:09 -0600

    used axios to perform get request to fetch recents posts and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,21 @@
+import { SET_RECENT_POSTS } from "./types";
+import axios from "axios";
+
 export function fetchReactPosts() {
     return function(dispatch) {
         //perform request here
-        console.log("yo! this is working");
+        // console.log("yo! this is working");
+        axios
+        .get("https://api.dailysmarty.com/posts")
+        .then( response => {
+            console.log("fetchReactPosts response", response.data.posts);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        })
+        .catch(error => {
+            console.log("error with fetchReactPosts", error);
+        });
     }
 }
---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,19 @@
+import {
+    SET_RECENT_POSTS
+} from "../actions/types";
+import recentPosts from "../components/recentPosts";
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function(state = INIT_STATE, action) {
+    switch (action.type) {
+        case SET_RECENT_POSTS:
+            
+            return [...state, action.payload]
+        default:
+            return state
+    }
+}
