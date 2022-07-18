8994c316fe240b32b3400f3a156fd796ddceff71
Christina Greenwood <greenwoodc@me.com>
2022-07-18 16:05:21 -0600

    added no-content to post links when post links are empty for post and fixed the grid the post link and applied flex styles to post links

--- src/components/post.js
+++ src/components/post.js
@@ -49,6 +49,9 @@ class Post extends Component {
     </div>  
       )      
       })
+      if (links == 0) {
+        return <div className='no-content'>No Post Link</div>
+      }
     return links;
 }
 
